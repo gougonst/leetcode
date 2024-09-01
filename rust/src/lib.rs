5455b36aee69923325a7c064566a847ca8eba93c
gougon <gougonst@gmail.com>
2024-09-02 00:07:20 +0800

    [NEW] 125 - Valid Palindrome

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -85,4 +85,14 @@ impl Solution {
         }
         ans
     }
+
+    // 125 - Valid Palindrome
+    pub fn is_palindrome(s: String) -> bool {
+        let iter = s
+            .chars()
+            .filter(|c| c.is_alphanumeric())
+            .map(|c| c.to_ascii_lowercase());
+
+        iter.clone().eq(iter.rev())
+    }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,7 @@
 use rust::Solution;
 
 fn main() {
-    let v = vec![7, 6, 4, 3, 1];
-    let ans = Solution::max_profit(v);
+    let s = String::from(" ");
+    let ans = Solution::is_palindrome(s);
     println!("{:?}", ans);
 }
